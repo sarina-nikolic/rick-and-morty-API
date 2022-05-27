ae8b529d585afc2181aafc19d1b61ae01b3df5d5
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2022-05-27 15:49:00 +0200

    Api fetch

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world!');
+fetch('https://rickandmortyapi.com/api/character')
+  .then(response => response.json())
+  .then(onData);
+
+function onData(data) {
+  const people = data.results;
+  const list = document.createElement('ul');
+  document.body.append(list);
+  people.forEach(person => {
+    const listItem = document.createElement('li');
+    listItem.className = 'card';
+
+    listItem.innerText = `${person.name} (${person.location})`;
+  });
+  console.log(people);
+}
